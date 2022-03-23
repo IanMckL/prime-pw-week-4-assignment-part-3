013f68afae3b654787d06aca7ee766080c3952aa
IanMckL <96411486+IanMckL@users.noreply.github.com>
2022-03-23 15:17:06 -0500

    Added answers

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,26 @@
-console.log('***** Cart Functions *****');
-// Make sure to test all functions here in the JS file!
-// We want to see how you are testing your code!!!
+let cart = []; 
+
+function addItem(item){ //Verified Working
+    cart.push(item);
+    return;
+}
+
+addItem('eggs'); //test -- Works
+console.log(cart.toString()); // test-- Works
+
+function listItems(target){   //Verified Working
+    for (i = 0; i < target.length; i++){
+        console.log(target[i])
+    }
+    return;
+}
+
+listItems(cart); // Test-- Works
+
+function empty(){ // Verified working
+    cart = [];
+}
+empty(); //Test-- Works
+console.log(cart) 
+
+
--- readme.md
+++ readme.md
@@ -29,7 +29,7 @@ Update the `cart.js` file to do the following:
   - console.log each individual item on a new line
 
 - Create a function called `empty`. It should:
-  - reset the `basket` to an empty array
+- reset the `basket` to an empty array
 
 > __IMPORTANT__
 > Make sure that you are writing code *in the file* to test every function that you write!
